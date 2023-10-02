8cb509fe7609f51fc5fb8200f93e16b0079ccaf7
Mehmet Karakuş <imeka4k@gmail.com>
2023-10-03 01:39:01 +0300

    Redis.Sentinel-InMemory-Distributed-Caching
    Gencay Yildiz Redis Kursu Redis.Sentinel-InMemory-Distributed-Caching

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        //[HttpGet("[Action]/{name}")]
+        //public void Set(string name)
+        //{
+        //    _memoryCache.Set("name", name);
+        //}
+
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    if(_memoryCache.TryGetValue<String>("name", out string name))
+        //    {
+        //        return name.Substring(3);
+        //    }
+        //    return "";
+        //}
+
+
+        [HttpGet("SetDate")]
+        public void SetDate()
+        {
+             _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("GetDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
---
+++ Redis.Pub.Sub.Subscribe.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = redis.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
